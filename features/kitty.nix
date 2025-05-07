e7eca25851f60245b3eb54a85ef9275268f8a6a2
ntrivunovic <ntrivunovic@croz.net>
2025-05-07 14:19:35 +0200

    Include arch config

--- features/kitty.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, config, ...}: 
-{
-  programs.kitty.enable = true;
-  programs.kitty.theme = "Tokyo Night";
-  programs.kitty.settings = {
-    font_family = "Fira Mono";
-    font_size = 12;
-  };
-}
--- fonts.nix
+++
@@ -1,9 +0,0 @@
-{ config, lib, pkgs, ...}:
-{
-  fonts.packages = with pkgs; [
-    (nerdfonts.override { fonts = ["FiraCode" "DroidSansMono" ]; })
-    proggyfonts
-    fira-mono
-  ];
-
-}
